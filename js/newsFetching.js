62aa66adea9da532a8c03899f4e4fe6a8a065096
HasanBakar <abubakar.cse@gmail.com>
2022-09-03 04:22:40 +0600

    different types of news navigation bar added

--- js/newsFetching.js
+++ js/newsFetching.js
@@ -1,15 +1,23 @@
-const navCatagories = () => {
+const navCatagories = (news) => {
+    news.navCatagories = true;
+
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(tamp => displayNavCatagories(tamp.data.news_category))
 }
 
-// const displayNavbar = navbar => {
-//     const { catagoryName } = navItem;
-//     navbar.forEach(navItem => {
-//         console.log(catagoryName)
+const displayNavCatagories = (dataArray) => {
 
-//     })
+    const container = document.getElementById('navigation_bar');
+    const home = document.getElementById('show_home');
+    home.classList.remove('hidden');
+    dataArray.forEach(differentNews => {
+        const { category_name } = differentNews;
+        const containerDiv = document.createElement('div');
+        containerDiv.innerHTML = ` 
+        <button  class="font-semibold text-lg mt-6 hover:text-white hover:rounded hover:bg-blue-600 px-1">${category_name}</button>
+        `;
+        container.appendChild(containerDiv);
+    });
+}
 
-// }
-navCatagories();
